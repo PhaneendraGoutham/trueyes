a032893121260c2e42e27efaac8ef8ef20505e8e
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2016-02-14 11:42:51 +0530

    Will rename angularjs module after this commit

--- trueyes-webfront/trueyes-webfront-ui/static/root/root.js
+++ trueyes-webfront/trueyes-webfront-ui/static/root/root.js
@@ -6,27 +6,14 @@ angular.module('root').config(function($stateProvider) {
         .state('root', {
             abstract: true,
             resolve: {
-                currentUser: function($http) {
-
-                	$http.get('user').then(function(success){
-                		alert('Eureka user call');
-                		console.info(success);
+                currentUser: function($http,$location) {
+                	return $http.get('user').then(function(success){
+                		console.debug('User Logged in');
+                		return success.data;
                 	},function(error){
-                		alert('user call failed');
-                		console.error(error);
+                		console.warn('user not logged in');
+                		return null;
                 	});
-                	
-                	$http.get('resource/list').success(function(data) {
-                		alert('REsource call works');
-                		console.log(data);
-                	}).error(function(error){
-                		alert('Resource Call Failed');
-                		console.error(error);
-                	});
-
-                    return {
-                        name: 'Sambhav'
-                    };
                 }
             },
             controller: function($scope, currentUser) {
