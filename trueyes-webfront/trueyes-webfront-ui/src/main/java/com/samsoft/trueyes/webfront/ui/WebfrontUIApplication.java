24854a5809e424e4cba1973a6486f5a8e1d1d57c
kumar.sambhav.jain@gmail.com <kumar.sambhav.jain@gmail.com>
2016-02-24 17:15:32 +0530

    Facebook Only Login

--- trueyes-webfront/trueyes-webfront-ui/src/main/java/com/samsoft/trueyes/webfront/ui/WebfrontUIApplication.java
+++ trueyes-webfront/trueyes-webfront-ui/src/main/java/com/samsoft/trueyes/webfront/ui/WebfrontUIApplication.java
@@ -9,13 +9,26 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -29,8 +42,11 @@ import org.springframework.web.util.WebUtils;
  */
 @SpringBootApplication
 @EnableZuulProxy
-@EnableOAuth2Sso
+@EnableOAuth2Client
 public class WebfrontUIApplication extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	protected OAuth2ClientContext oauth2ClientContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebfrontUIApplication.class, args);
@@ -69,8 +85,41 @@ public class WebfrontUIApplication extends WebSecurityConfigurerAdapter {
 				.and()
 				.csrf().csrfTokenRepository(csrfTokenRepository())
 				.and()
-				.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+				.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
 		//@formatter:on
+	}
+	
+	
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(
+			OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
+	private Filter ssoFilter() {
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				"/login/facebook");
+		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+		facebookFilter.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(
+				new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId()));
+		return facebookFilter;
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook.client")
+	OAuth2ProtectedResourceDetails facebook() {
+		return new AuthorizationCodeResourceDetails();
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook.resource")
+	ResourceServerProperties facebookResource() {
+		return new ResourceServerProperties();
 	}
 
 	private Filter csrfHeaderFilter() {
