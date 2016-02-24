6986c0d2e4132f1a03216808ff15ed466be25336
kumar.sambhav.jain@gmail.com <kumar.sambhav.jain@gmail.com>
2016-02-24 20:21:07 +0530

    Same old google login problem

--- trueyes-webfront/trueyes-webfront-ui/src/main/java/com/samsoft/trueyes/webfront/ui/WebfrontUIApplication.java
+++ trueyes-webfront/trueyes-webfront-ui/src/main/java/com/samsoft/trueyes/webfront/ui/WebfrontUIApplication.java
@@ -1,6 +1,11 @@
 package com.samsoft.trueyes.webfront.ui;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -18,6 +23,8 @@ import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -27,29 +34,89 @@ import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticat
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
+
+class ClientResources {
+	private OAuth2ProtectedResourceDetails client = new AuthorizationCodeResourceDetails();
+	private ResourceServerProperties resource = new ResourceServerProperties();
+
+	public OAuth2ProtectedResourceDetails getClient() {
+		return client;
+	}
+
+	public ResourceServerProperties getResource() {
+		return resource;
+	}
+}
 
 /**
  * @author sambhav.jain
  *
  */
-@SpringBootApplication
 @EnableZuulProxy
+@SpringBootApplication
+@RestController
 @EnableOAuth2Client
+@EnableAuthorizationServer
+@Order(6)
 public class WebfrontUIApplication extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	protected OAuth2ClientContext oauth2ClientContext;
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.antMatcher("/me")
+				.authorizeRequests().anyRequest().authenticated();
+			// @formatter:on
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebfrontUIApplication.class, args);
+	}
+
+	@Autowired
+	OAuth2ClientContext oauth2ClientContext;
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		//@formatter:off
+		http
+			.logout()
+			.and()
+			.antMatcher("/**").authorizeRequests()
+			.antMatchers(	
+					"/index.html",
+					"/home.html", 
+					"/", 
+					"/login/**", 
+					"/**/*.html", 
+					"/**/*.less", 
+					"/**/*.css", 
+					"/**/*.js").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.csrf().csrfTokenRepository(csrfTokenRepository())
+				.and()
+				.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+		//@formatter:on
 	}
 
 	@Override
@@ -63,63 +130,6 @@ public class WebfrontUIApplication extends WebSecurityConfigurerAdapter {
 					"/**/*.less", 
 					"/**/*.js");
 		//@formatter:on
-	}
-
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		//@formatter:off
-		http
-			.logout()
-			.and()
-			.antMatcher("/**").authorizeRequests()
-			.antMatchers(	
-					"/index.html",
-					"/home.html", 
-					"/", 
-					"/login", 
-					"/**/*.html", 
-					"/**/*.less", 
-					"/**/*.css", 
-					"/**/*.js").permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.csrf().csrfTokenRepository(csrfTokenRepository())
-				.and()
-				.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
-				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-		//@formatter:on
-	}
-	
-	
-	@Bean
-	public FilterRegistrationBean oauth2ClientFilterRegistration(
-			OAuth2ClientContextFilter filter) {
-		FilterRegistrationBean registration = new FilterRegistrationBean();
-		registration.setFilter(filter);
-		registration.setOrder(-100);
-		return registration;
-	}
-
-	private Filter ssoFilter() {
-		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
-				"/login/facebook");
-		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
-		facebookFilter.setRestTemplate(facebookTemplate);
-		facebookFilter.setTokenServices(
-				new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId()));
-		return facebookFilter;
-	}
-
-	@Bean
-	@ConfigurationProperties("facebook.client")
-	OAuth2ProtectedResourceDetails facebook() {
-		return new AuthorizationCodeResourceDetails();
-	}
-
-	@Bean
-	@ConfigurationProperties("facebook.resource")
-	ResourceServerProperties facebookResource() {
-		return new ResourceServerProperties();
 	}
 
 	private Filter csrfHeaderFilter() {
@@ -146,6 +156,52 @@ public class WebfrontUIApplication extends WebSecurityConfigurerAdapter {
 		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
 		repository.setHeaderName("X-XSRF-TOKEN");
 		return repository;
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook")
+	ClientResources facebook() {
+		return new ClientResources();
+	}
+
+	@Bean
+	@ConfigurationProperties("google")
+	ClientResources google() {
+		return new ClientResources();
+	}
+
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
+	private Filter ssoFilter() {
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+		filters.add(ssoFilter(facebook(), "/login/facebook"));
+		filters.add(ssoFilter(google(), "/login/google"));
+		filter.setFilters(filters);
+		return filter;
+	}
+
+	private Filter ssoFilter(ClientResources client, String path) {
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				path);
+		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+		facebookFilter.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(
+				new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId()));
+		return facebookFilter;
+	}
+
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		return map;
 	}
 
 }
